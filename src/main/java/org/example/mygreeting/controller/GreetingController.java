a54a35126840ae983e64a3b67ce48682f0908ae1
Harshit Goyal <harshitkg2003@gmail.com>
2025-03-03 15:04:21 +0530

    UC2

--- src/main/java/org/example/mygreeting/controller/GreetingController.java
+++ src/main/java/org/example/mygreeting/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package org.example.mygreeting.controller;
 
+import org.example.mygreeting.service.GreetingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greeting")
 public class GreetingController {
 
+    private final GreetingService greetingService;
+
+    // Constructor-based Dependency Injection
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     @GetMapping("/message")
     public String getMessage() {
-        return "Hello, Welcome to the Greeting App!";
+        return greetingService.getGreetingMessage();
     }
 }
