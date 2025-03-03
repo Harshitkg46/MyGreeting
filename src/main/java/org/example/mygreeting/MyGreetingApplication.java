3b3ac7158aeea209612115889a385ddad3080c06
Harshit Goyal <harshitkg2003@gmail.com>
2025-03-03 14:56:49 +0530

    UC1

---
+++ src/main/java/org/example/mygreeting/MyGreetingApplication.java
@@ -0,0 +1,13 @@
+package org.example.mygreeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGreetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGreetingApplication.class, args);
+    }
+
+}
